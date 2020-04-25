2e21fa94aa6af92c9fc64e9de7ffa89f12117364
skylar112 <62074392+skylar112@users.noreply.github.com>
2020-04-25 16:45:12 -0400

    Add files via upload

--- script.js
+++ script.js
@@ -1,4 +1,7 @@
 $(document).ready(function(){
+  var now = moment().format('MMMM Do YYYY');
+  console.log(now);
+  $("#today").text(now);
 
   $(".save-btn").on("click", function(){
     var activity = $(this).siblings(".description").val();
@@ -10,27 +13,45 @@ $(document).ready(function(){
     console.log(localStorage);
     
   })
-  var currentHour = momemt().hours()
-  var blockHour = parseInt($(this).attr("id").split("-")[1])
   //create a function that compares our hour to moments hour.  If statement that add class of past, present or future.
+  function hourChecker() {
+    var currentHour = moment().hours();
+    $(".time-block").each(function() {
+      var blockHour = parseInt($(this).attr("id").split("-")[1]);
+     
+      if (blockHour < currentHour) {
+        $(this).addClass("past");
+      }
+      else if (blockHour === currentHour){
+        $(this).removeClass("past")
+        $(this).addClass("present");
+      }
+       else {
+        $(this).removeClass("past")
+        $(this).removeClass("present")
+        $(this).addClass("future");
+        }
+    })
+  }
+  hourChecker();
 
-  $("#hr-9 .description").val(localStorage.getItem("hr-9"))
+  $("#hr-9 .description").val(localStorage.getItem("hr-9"));
 
-  $("#hr-10 .description").val(localStorage.getItem("hr-10"))
+  $("#hr-10 .description").val(localStorage.getItem("hr-10"));
 
-  $("#hr-11 .description").val(localStorage.getItem("hr-11"))
+  $("#hr-11 .description").val(localStorage.getItem("hr-11"));
 
-  $("#hr-12 .description").val(localStorage.getItem("hr-12"))
+  $("#hr-12 .description").val(localStorage.getItem("hr-12"));
 
-  $("#hr-1 .description").val(localStorage.getItem("hr-1"))
+  $("#hr-1 .description").val(localStorage.getItem("hr-1"));
 
-  $("#hr-2 .description").val(localStorage.getItem("hr-2"))
+  $("#hr-2 .description").val(localStorage.getItem("hr-2"));
 
-  $("#hr-3 .description").val(localStorage.getItem("hr-3"))
+  $("#hr-3 .description").val(localStorage.getItem("hr-3"));
 
-  $("#hr-4 .description").val(localStorage.getItem("hr-4"))
+  $("#hr-4 .description").val(localStorage.getItem("hr-4"));
 
-  $("#hr-5 .description").val(localStorage.getItem("hr-5"))
+  $("#hr-5 .description").val(localStorage.getItem("hr-5"));
 
   
     
